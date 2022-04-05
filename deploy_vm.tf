078f069c7472abcb19cb8df4d685bc5e2207221e
student <tfdc-student@flintmail.com>
2022-04-05 07:53:41 +0000

    solutions

--- deploy_vm.tf
+++ deploy_vm.tf
@@ -16,24 +16,21 @@ provider "vsphere" {
   allow_unverified_ssl = true
 }
 
-locals  {
-  Pod = "PodXX"
-}
-
 module "pod_vms" {
   source    = "./vm_module"
-  folder = local.Pod
-  hostname = "${local.Pod}-VM"
-  vm_name = "${local.Pod}-VM"
+  folder = var.Pod
+  hostname = "${var.Pod}-VM"
+  vm_name = "${var.Pod}-VM"
   cpu       = 2
   ram       = 1024
-  DHCP      = false
+  DHCP      = true
   ipv4_address = "10.156.32.87"
   ipv4_gateway = "10.156.32.254"
   ipv4_netmask = 24
 }
 
+/*
 output "vars" {
   value = regexall("(?:variable\\s\\W)(.*\\w)",file("vm_module/variables.tf"))
 }
-
+*/
--- variables.tf
+++ variables.tf
@@ -1,4 +1,10 @@
 variable "password" {
     type = string
-    sensitive = true
+    description = "vSphere password"
+}
+
+variable "Pod" {
+    type = string
+    default = "PodXX"
+    description = "Change XX with your pod number. If below 10, use 0X notation"
 }
