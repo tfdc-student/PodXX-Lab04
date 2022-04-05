d7024706a2b4317ed2cc5c2c75c17fc80b41e75c
student <tfdc-student@flintmail.com>
2022-04-05 08:09:48 +0000

    init

--- deploy_vm.tf
+++ deploy_vm.tf
@@ -16,21 +16,13 @@ provider "vsphere" {
   allow_unverified_ssl = true
 }
 
-module "pod_vms" {
-  source    = "./vm_module"
-  folder = var.Pod
-  hostname = "${var.Pod}-VM"
-  vm_name = "${var.Pod}-VM"
-  cpu       = 2
-  ram       = 1024
-  DHCP      = true
-  ipv4_address = "10.156.32.87"
-  ipv4_gateway = "10.156.32.254"
-  ipv4_netmask = 24
-}
-
 /*
 output "vars" {
   value = regexall("(?:variable\\s\\W)(.*\\w)",file("vm_module/variables.tf"))
 }
 */
+
+
+
+
+
